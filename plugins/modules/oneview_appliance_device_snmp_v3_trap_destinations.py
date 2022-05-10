47d03f86fa178258536318b39871111a2d13648d
nabhajit-ray <62326750+nabhajit-ray@users.noreply.github.com>
2022-05-10 16:50:48 +0530

    Update oneview_appliance_device_snmp_v3_trap_destinations.py

--- plugins/modules/oneview_appliance_device_snmp_v3_trap_destinations.py
+++ plugins/modules/oneview_appliance_device_snmp_v3_trap_destinations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 ###
-# Copyright (2021) Hewlett Packard Enterprise Development LP
+# Copyright (2021) Hewlett Packard Enterprise Development LP1
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # You may not use this file except in compliance with the License.
